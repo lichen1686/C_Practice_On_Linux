64a189cd2ec45bc31995b312beed05d5fe97fe0c
lichen <1176343828@qq.com>
2023-04-12 19:35:00 +0800

    switch num by pointer

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void switch_num(int *a, int *b)
+{
+    int num;
+    num = *b;
+    *b = *a;
+    *a = num;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int a = 10;
+    int b = 20;
+    printf("a = %d, b = %d\n", a, b);
+    switch_num(&a, &b);
+    printf("a = %d, b = %d", a, b);
     return 0;
 }
